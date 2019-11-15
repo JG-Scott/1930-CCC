88be83029bdc571b27268f8baf5d67d37839d039
Sean <sean.joseph.green@gmail.com>
2019-11-15 09:13:48 -0800

    User login now reads to DB

---
+++ scripts/firebase_config_ccc.js
@@ -0,0 +1,15 @@
+  // Your web app's Firebase configuration
+       // Your web app's Firebase configuration
+       var firebaseConfig = {
+        apiKey: "AIzaSyBSaoPBhAEoTNGd1Z_sNHyWukRG1K94yzg",
+        authDomain: "the-car-cost-calculator.firebaseapp.com",
+        databaseURL: "https://the-car-cost-calculator.firebaseio.com",
+        projectId: "the-car-cost-calculator",
+        storageBucket: "the-car-cost-calculator.appspot.com",
+        messagingSenderId: "415596279463",
+        appId: "1:415596279463:web:796e5f4ed5122881354d82"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      const db = firebase.firestore();
+    
