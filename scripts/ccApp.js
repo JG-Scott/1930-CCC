f8debf492453483ad3c7c83740685bc7a38e919d
Kyle Herrmann <swisskyle107@gmail.com>
2019-12-05 08:36:46 -0800

    Main code for complete calculator

--- scripts/ccApp.js
+++ scripts/ccApp.js
@@ -196,58 +196,80 @@ function getDieselPrice() {
 ///////////////////////////////////////////////////////////////////////////////
 function goVehicleProfile() {
 
-    document.getElementById("display").innerHTML = "<h2>Select a vehicle:</h2>"
-    document.getElementById("b1").innerHTML = '<button type="button" class="btn btn-success" onclick="getCoupMPG()">Coup</button>';
+    document.getElementById("ccPrompt").innerHTML = "<h2>Select a vehicle</h2>"
+    document.getElementById("b1").innerHTML = '<button type="button" class="btn btn-success" onclick="getCoupMPG()">Coupe</button>';
     document.getElementById("b2").innerHTML = '<button type="button" class="btn btn-success" onclick="getSUVMPG()">SUV</button>';
     document.getElementById("b3").innerHTML = '<button type="button" class="btn btn-success" onclick="getSedanMPG()">Sedan</button>';
     document.getElementById("b4").innerHTML = '<button type="button" class="btn btn-success" onclick="getTruckMPG()">Truck</button>';
+    document.getElementById("current").innerHTML = "";
+    document.getElementById("start").innerHTML = "";
 }
 
 function goCommute() {
 
-    document.getElementById("display").innerHTML = "<h2>Select a Trip:</h2>"
+    document.getElementById("ccPrompt").innerHTML = "<h2>Select a Trip</h2>"
     document.getElementById("b1").innerHTML = '<button type="button" class="btn btn-success" onclick="getLangleyDistance()">BCIT to Langley</button>';
     document.getElementById("b2").innerHTML = '<button type="button" class="btn btn-success" onclick="getAbbotsfordDistance()">BCIT to Abbotsford</button>';
     document.getElementById("b3").innerHTML = '<button type="button" class="btn btn-success" onclick="getPMDistance()">BCIT to Port Moody</button>';
     document.getElementById("b4").innerHTML = "";
+    document.getElementById("current").innerHTML = "";
+    document.getElementById("start").innerHTML = "";
 }
 
 function goGas() {
 
-    document.getElementById("display").innerHTML = "<h2>Select gas grade:</h2>"
+    document.getElementById("ccPrompt").innerHTML = "<h2>Select gas grade</h2>"
     document.getElementById("b1").innerHTML = '<button type="button" class="btn btn-success" onclick="getRegPrice()">Regular</button>';
     document.getElementById("b2").innerHTML = '<button itype="button" class="btn btn-success" onclick="getMidPrice()">Mid-Gade</button>';
     document.getElementById("b3").innerHTML = '<button type="button" class="btn btn-success" onclick="getPremPrice()">Premium</button>';
-    document.getElementById("b4").innerHTML = '<button type="button" class="btn btn-success" onclick="getDieselPrice()">Diesel</button>'
+    document.getElementById("b4").innerHTML = '<button type="button" class="btn btn-success" onclick="getDieselPrice()">Diesel</button>';
+    document.getElementById("current").innerHTML = "";
+    document.getElementById("start").innerHTML = "";
 }
 
 ////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////Code That Builds The Display/////////////////
 ////////////////////////////////////////////////////////////////////////////
 function getCurrentProfile() {
-
-    document.getElementById("display").innerHTML = "<h1>Car Type: " + carType + "</h1><h2>Commute: " + tripDistance + " miles</h2><h2>Fuel: " + gasPrice + "</h2>";
-    document.getElementById("b1").innerHTML = "<button type='button' class='btn btn-success' onclick='goVehicleProfile()'>NEW</button>";
+    
+    document.getElementById("ccPrompt").innerHTML = "<h2>Car Type: " + carType + "</h2><h2>Commute: " + tripDistance + " miles</h2><h2>Fuel: " + gasPrice + "</h2>";
+    document.getElementById("current").innerHTML = "";
+    document.getElementById("home").innerHTML = "";
+    document.getElementById("b1").innerHTML = "";
     document.getElementById("b2").innerHTML = "";
     document.getElementById("b3").innerHTML = "";
     document.getElementById("b4").innerHTML = "";
+    document.getElementById("start").innerHTML = '<button type="button" class="btn btn-success" onclick="getResult()">Next</button>';
+    document.getElementById("home").innerHTML = '<button type="button" class="btn btn-success" onclick="window.location.href="main.html"">Home</button>';
 }
 
-// function getUserDetails() {
-//     firebase.auth().onAuthStateChanged(function (user) {
-//         db.collection("users").doc(user.uid).onSnapshot(function (doc) {
-//             carType = doc.get("car");
-//             tripDistance = doc.get("commute");
-//             gasPrice = doc.get("gasType)");
-//             console.log(gasPrice);
-//         });
-//     })
-// };
 function newProfile() {
 
-    document.getElementById("display").innerHTML = "<h1>No Profile</h1>";
-    document.getElementById("b1").innerHTML = "<button type='button' class='btn btn-success' onclick='goVehicleProfile()'>NEW</button>";
-    document.getElementById("b2").innerHTML = "";
-    document.getElementById("b3").innerHTML = "";
-    document.getElementById("b4").innerHTML = "";
+    document.getElementById("ccPrompt").innerHTML = "<h1>No Profile</h1>";
+    document.getElementById("current").innerHTML = "";
+    document.getElementById("home").innerHTML = "";
+    document.getElementById("start").innerHTML = "<button type='button' id='btn1' class='btn btn-success' onclick='goVehicleProfile()'>NEW</button>";
+}
+
+function getResult() {
+
+    //Store cost per time frame.
+    var weekCost;
+    var monthCost;
+    var yearCost;
+
+    //Determine cost per gallon.
+    var costPerG = gasPrice * 0.26;
+    //Determine cost per trip.
+    var tripCost = (tripDistance / MPG) * gasPrice;
+
+    //Assume trip is made 3 times per week.
+    var weekCost =  "<h2>Per Week: $" + (Math.round((tripCost * 3) * 100) / 100).toFixed(2); + "</h2>";
+    var monthCost = "<h2>Per Month: $" + (Math.round((tripCost * 15) * 100) / 100).toFixed(2); + "</h2>";
+    var yearCost = "<h2>Per Year: $" + (Math.round((tripCost * 180) * 100) / 100).toFixed(2); + "</h2>";
+
+    //Display results.
+    document.getElementById("ccPrompt").innerHTML = "<h2>Gas Cost</h2><br />" + weekCost + monthCost + yearCost;
+    document.getElementById("home").innerHTML = "<button type='button' class='btn btn-success' onclick='window.location.href='main.html'>Home</button>";
+    
 }
