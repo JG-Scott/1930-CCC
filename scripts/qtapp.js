219ad8e97dac0288c20c9a5abe6d71a457fd0f9a
Sean <sean.joseph.green@gmail.com>
2019-12-05 19:14:03 -0800

    added the logout button and made the quick trip calculator for long distance trips.

--- scripts/qtapp.js
+++ scripts/qtapp.js
@@ -2,13 +2,51 @@ var gasPrice;
 var carWeight;
 var tripDistance;
 var stage;
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////Code that rebuilds the buttons////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+function goVehicle() {
+
+    document.getElementById("display").innerHTML = "Select a vehicle:";
+    document.getElementById("b1").innerHTML = '<button type="button" class="btn btn-success" onclick="getCoupWeight()">Coup</button>';
+    document.getElementById("b2").innerHTML = '<button type="button" class="btn btn-success" onclick="getSUVWeight()">SUV</button>';
+    document.getElementById("b3").innerHTML = '<button type="button" class="btn btn-success" onclick="getSedanWeight()">Sedan</button>';
+    document.getElementById("b4").innerHTML = '<button type="button" class="btn btn-success" onclick="getTruckWeight()">Truck</button>';
+}
+function goTrip() {
+
+    document.getElementById("display").innerHTML = "Select a Trip:";
+    document.getElementById("b1").innerHTML = '<button type="button" class="btn btn-success" onclick="getLADistance()">Los Angeles</button>';
+    document.getElementById("b2").innerHTML = '<button type="button" class="btn btn-success" onclick="getThunderBayDistance()">Thunder Bay</button>';
+    document.getElementById("b3").innerHTML = '<button type="button" class="btn btn-success" onclick="getMCDistance()">Mexico City</button>';
+    document.getElementById("b4").innerHTML = "";
+}
+function goGas() {
+
+    document.getElementById("display").innerHTML = "Select gas grade:";
+    document.getElementById("b1").innerHTML = '<button type="button" class="btn btn-success" onclick="getRegPrice()">Regular</button>';
+    document.getElementById("b2").innerHTML = '<button itype="button" class="btn btn-success" onclick="getMidPrice()">Mid-Gade</button>';
+    document.getElementById("b3").innerHTML = '<button type="button" class="btn btn-success" onclick="getPremPrice()">Premium</button>';
+    document.getElementById("b4").innerHTML = '<button type="button" class="btn btn-success" onclick="getDieselPrice()">Diesel</button>'
+}
+function goResult() {
+    db.collection("empty").doc("empty").onSnapshot(function (doc) {  
+    document.getElementById("display").innerHTML = doc.get("empty") + "Your trip will cost:  $" + ((tripDistance / carWeight) * (gasPrice * 3.875)).toFixed(2);
+    document.getElementById("b1").innerHTML = '<button type="button" class="btn btn-success" onclick="goVehicle()">New Trip</button>';
+    document.getElementById("b2").innerHTML = '';
+    document.getElementById("b3").innerHTML = '';
+    document.getElementById("b4").innerHTML = ''
+
+});
+   
+}
 //////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////Get vehicle weight from the DB//////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
 function getCoupWeight() {
 
     db.collection("Vehicles").doc("Coup").onSnapshot(function (doc) {
-        carWeight = doc.get("weight");
+        carWeight = doc.get("mpg");
         console.log(carWeight);
         goTrip();
     });
@@ -17,7 +55,7 @@ function getCoupWeight() {
 function getSUVWeight() {
 
     db.collection("Vehicles").doc("SUV").onSnapshot(function (doc) {
-        carWeight = doc.get("weight");
+        carWeight = doc.get("mpg");
         console.log(carWeight);
         goTrip();
     });
@@ -26,7 +64,7 @@ function getSUVWeight() {
 function getSedanWeight() {
 
     db.collection("Vehicles").doc("Sedan").onSnapshot(function (doc) {
-        carWeight = doc.get("weight");
+        carWeight = doc.get("mpg");
         console.log(carWeight);
         goTrip();
     });
@@ -36,7 +74,7 @@ function getSedanWeight() {
 function getTruckWeight() {
 
     db.collection("Vehicles").doc("Truck").onSnapshot(function (doc) {
-        carWeight = doc.get("weight");
+        carWeight = doc.get("mpg");
         console.log(carWeight);
         goTrip();
     });
@@ -44,27 +82,27 @@ function getTruckWeight() {
 /////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////Get Distances From the DB/////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////
-function getLangleyDistance() {
+function getLADistance() {
 
-    db.collection("Trips").doc("BCITtoLangley").onSnapshot(function (doc) {
-        tripDistance = doc.get("Distance(KM)");
-        console.log(tripDistance);
+    db.collection("Trips").doc("LA").onSnapshot(function (doc) {
+        tripDistance = doc.get("miles");
+        console.log(doc.get("miles"));
         goGas();
     });
 }
-function getAbbotsfordDistance() {
+function getThunderBayDistance() {
 
-    db.collection("Trips").doc("BCITtoAbbotsford").onSnapshot(function (doc) {
-        tripDistance = doc.get("Distance(KM)");
-        console.log(tripDistance);
+    db.collection("Trips").doc("ThunderBay").onSnapshot(function (doc) {
+        tripDistance = doc.get("miles");
+        console.log(doc.get("miles"));
         goGas();
     });
 }
-function getPMDistance() {
+function getMCDistance() {
 
-    db.collection("Trips").doc("BCITtoPortMoody").onSnapshot(function (doc) {
-        tripDistance = doc.get("Distance(KM)");
-        console.log(tripDistance);
+    db.collection("Trips").doc("Mexico City").onSnapshot(function (doc) {
+        tripDistance = doc.get("miles");
+        console.log(doc.get("miles"));
         goGas();
     });
 }
@@ -90,6 +128,7 @@ function getMidPrice() {
     });
 }
 
+
 function getPremPrice() {
 
     db.collection("Gas").doc("Premium").onSnapshot(function (doc) {
@@ -98,52 +137,11 @@ function getPremPrice() {
         goResult();
     });
 }
+
 function getDieselPrice() {
     db.collection("Gas").doc("Diesel").onSnapshot(function (doc) {
         gasPrice = doc.get("Price(CA)");
         console.log(gasPrice);
         goResult();
     });
-}
-
-
-
-
-////////////////////////////////////////////////////////////////////////////
-///////////////////////////////Code That Writes/////////////////////////////
-////////////////////////////////////////////////////////////////////////////
-
-// db.collection("users").doc(user.uid).update({
-//     "Car Type": document.getElementById('carTypeinfo').value,
-// });
-
-///////////////////////////////////////////////////////////////////////////////
-/////////////////////////Code that rebuilds the buttons////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-function goVehicle() {
-
-    document.getElementById("display").innerHTML = "Select a vehicle:"
-    document.getElementById("b1").innerHTML = '<button type="button" class="btn btn-success" onclick="getCoupWeight()">Coup</button>';
-    document.getElementById("b2").innerHTML = '<button type="button" class="btn btn-success" onclick="getSUVWeight()">SUV</button>';
-    document.getElementById("b3").innerHTML = '<button type="button" class="btn btn-success" onclick="getSedanWeight()">Sedan</button>';
-    document.getElementById("b4").innerHTML = '<button type="button" class="btn btn-success" onclick="getTruckWeight()">Truck</button>';
-}
-function goTrip() {
-
-    document.getElementById("display").innerHTML = "Select a Trip:"
-    document.getElementById("b1").innerHTML = '<button type="button" class="btn btn-success" onclick="getLangleyDistance()">BCIT to Langley</button>';
-    document.getElementById("b2").innerHTML = '<button type="button" class="btn btn-success" onclick="getAbbotsfordDistance()">BCIT to Abbotsford</button>';
-    document.getElementById("b3").innerHTML = '<button type="button" class="btn btn-success" onclick="getPMDistance()">BCIT to Port Moody</button>';
-    document.getElementById("b4").innerHTML = "";
-}
-function goGas() {
-
-    document.getElementById("display").innerHTML = "Select gas grade:"
-    document.getElementById("b1").innerHTML = '<button type="button" class="btn btn-success" onclick="getRegPrice()">Regular</button>';
-    document.getElementById("b2").innerHTML = '<button itype="button" class="btn btn-success" onclick="getMidPrice()">Mid-Gade</button>';
-    document.getElementById("b3").innerHTML = '<button type="button" class="btn btn-success" onclick="getPremPrice()">Premium</button>';
-    document.getElementById("b4").innerHTML = '<button type="button" class="btn btn-success" onclick="getDieselPrice()">Diesel</button>'
-}
-function goResult() {
-    document.getElementById("display").innerHTML = "Gas: " + gasPrice + " Distance: " + tripDistance + " Weight: " + carWeight
 }
